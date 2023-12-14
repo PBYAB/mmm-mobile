4d2f4adf191d5766f5a481ca7db424580f1ef14d
PBYAB <michalzajkowski71@gmail.com>
2023-12-14 21:21:05 +0100

    Revert "openapi generator"
    This reverts commit 9c5588dc6b5f884efd9ac1e82c6fcef5c3d8188d.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,10 +72,4 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
