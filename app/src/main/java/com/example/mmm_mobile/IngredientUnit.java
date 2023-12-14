74f71cc409beeb1eee7af8b7d985d78784d5ebe7
azdan <olczali4@gmail.com>
2023-12-14 21:42:25 +0100

    dodawanie i edytowanie przepisów, jeszcze nie działa

---
+++ app/src/main/java/com/example/mmm_mobile/IngredientUnit.java
@@ -0,0 +1,5 @@
+package com.example.mmm_mobile;
+
+public enum IngredientUnit {
+    G, ML
+}
--- app/src/main/java/com/example/mmm_mobile/MainActivity.kt
+++ app/src/main/java/com/example/mmm_mobile/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.mmm_mobile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mmm_mobile.ui.theme.MmmmobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Button(
+                        onClick = {
+                            val intent = Intent(this, AddRecipeActivity::class.java)
+                            startActivity(intent)
+                        },
+                        modifier = Modifier
+                    ) {
+                        Text("Go to AddRecipeActivity")
+                    }
                 }
             }
         }
