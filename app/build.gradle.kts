2f08c5957f206fac1b1a6a067e7180950a6eeb9f
michalniebrzydowski <michal.niebrzydowski@hotmail.com>
2023-12-19 11:35:54 +0100

    generated openapi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,4 +96,10 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.moshi:moshi-adapters:1.14.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 }
