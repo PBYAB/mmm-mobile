83f58647f891c72e0292f9a7f2f67ba3c3e84756
michalniebrzydowski <michal.niebrzydowski@hotmail.com>
2024-01-15 20:32:06 +0100

    sprawdzanie w api czy wyslac powiadomienie

--- app/src/main/java/com/example/mmm_mobile/MainActivity.kt
+++ app/src/main/java/com/example/mmm_mobile/MainActivity.kt
@@ -57,10 +57,13 @@ import com.example.mmm_mobile.utils.NotificationReceiver
 import com.example.mmm_mobile.utils.NotificationScheduler
 
 
+private const val NOTIFICATION_MINUTES_INTERVAL = 1L
+private const val MINUTE_TO_MILLIS = 60 * 1000L
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        NotificationScheduler.setReminder(this, NotificationReceiver::class.java, 1 * 60 * 1000)
+        NotificationScheduler.setReminder(this, NotificationReceiver::class.java, NOTIFICATION_MINUTES_INTERVAL * MINUTE_TO_MILLIS)
 
         setContent {
             MmmmobileTheme {
